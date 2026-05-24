39e390171290dd5ca5ad6c27b670a5919c6a0365
Cristopher Ospina <180292639+cristopherr26@users.noreply.github.com>
2026-05-23 21:57:28 -0500

    Conexión con el spring cloud config donde viven las rutas necesarias

---
+++ Rapizzas-Gateway/src/main/java/co/com/rapizzas/initializer/RapizzasGatewayApplication.java
@@ -0,0 +1,13 @@
+package co.com.rapizzas.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapizzasGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapizzasGatewayApplication.class, args);
+	}
+
+}
---
+++ Rapizzas-Gateway/src/test/java/co/com/rapizzas/initializer/RapizzasGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.rapizzas.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapizzasGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
